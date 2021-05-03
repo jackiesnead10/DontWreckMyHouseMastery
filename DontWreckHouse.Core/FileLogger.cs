9c6bcd6722415acd0d9922b5837187be06ec4c6f
jackiesnead10 <jackiesneadthe3rd@outlook.com>
2021-05-02 21:21:46 -0700

    final version

---
+++ DontWreckHouse.Core/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DontWreckHouse.Core
+{
+    public class FileLogger : ILogger
+    {
+        private string _filepath;
+        public FileLogger(string filepath)
+        {
+            _filepath = Path.Combine(filepath, DateTime.Today.ToString());
+        }
+        public void Log(string log)
+        {
+            using StreamWriter writer = new StreamWriter(_filepath);
+            writer.WriteLine(log);
+
+         
+
+       
+        }
+    }
+}
---
+++ DontWreckMyHouse/NinjectContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using System.IO;
+using DontWreckHouse.Core;
+using DontWreckHouse.DAL;
+
+namespace DontWreckMyHouse.UI
+{
+    class NinjectContainer
+    {
+        public static StandardKernel Kernal { get; private set; }
+
+        public static void Configure()
+        {
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string reservationsFileDirectory = Path.Combine(projectDirectory, "dont-wreck-my-house-data", "reservations");
+            string guestsFilePath = Path.Combine(projectDirectory, "dont-wreck-my-house-data", "guests.csv");
+            string hostsFilePath = Path.Combine(projectDirectory, "dont-wreck-my-house-data", "hosts.csv");
+            string fileLoggerPath = Path.Combine(projectDirectory, "dont-wreck-my-house-data", "Logs");
+
+            Kernal = new StandardKernel();
+            Kernal.Bind<IReservationsFileRepository>().To<ReservationFileRepository>().WithConstructorArgument("filePath", reservationsFileDirectory);
+            Kernal.Bind<IGuestFileRepository>().To<GuestFileRepository>().WithConstructorArgument("filePath", guestsFilePath);
+            Kernal.Bind<IHostFileRepository>().To<HostFileRepository>().WithConstructorArgument("filePath", hostsFilePath);
+            Kernal.Bind<ILogger>().To<FileLogger>().WithConstructorArgument("filepath", Path.Combine(fileLoggerPath, "Logs"));
+
+        }
+    }
+}
